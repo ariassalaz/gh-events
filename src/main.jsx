ddfaed98c65b989e8191b80e3d6c849b2e8801d2
Ricardo <alu.22130809@correo.itlalaguna.edu.mx>
2026-02-19 21:29:12 -0600

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// comment
-// comment 2
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
